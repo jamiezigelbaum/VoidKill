b3c1b05309070e67b32f7bc868cea2514cb7af8a
Jamie Zigelbaum <jamie@zigelbaumcoelho.com>
2013-03-05 14:36:44 -0500

    adds some info in the comments of testApp.h

--- emptyExample/src/testApp.h
+++ emptyExample/src/testApp.h
@@ -1,11 +1,40 @@
 #pragma once
 
+ /*
+  * HELLO VIEWER
+  * HELLO READER
+  *
+  * This performance is called Void Kill (). 
+  *
+  * If you are here in person you will see me working at a desk with a computer, 
+  * keyboard, etc. writing some software in a gallery at the Spring Break Art Show in New York.
+  * Or maybe it's after the performance and you are just seeing the remains, reading a quiet screen.
+  *
+  * If you are reading this somewhere else, you are perhaps on github, looking at the code
+  * on your computer, or reading it some other way after the performance..
+  *
+  * Github repo https://github.com/jamiezigelbaum/VoidKill
+  *
+  */
+
+
+
 #include "ofMain.h"
 
+
+
+
 class testApp : public ofBaseApp{
-	public:
-		void setup();
-		void update();
+
+  public:
+    void setup();
+    void update();
 		
-        void preflightCheck();
+    void preflightCheck();
+    void takeoff();
+    void exitAirspace();
+    void setDestination();
+    
+
+    
 };
