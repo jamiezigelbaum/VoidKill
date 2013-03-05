ce8f59cba2d11a4715a58c990db78179eed0c545
Jamie Zigelbaum <jamie@zigelbaumcoelho.com>
2013-03-05 14:54:54 -0500

    adds more methods, including void kill

--- emptyExample/src/testApp.cpp
+++ emptyExample/src/testApp.cpp
@@ -6,12 +6,20 @@ void testApp::setup(){
 
 }
 
+
+//--------------------------------------------------------------
+void testApp::update(){
+
+}
+
+
 //--------------------------------------------------------------
 void testApp::preflightCheck(){
     // This seems a good thing, I'll make sure I won't explode when I take off
 }
 
-//--------------------------------------------------------------
-void testApp::update(){
 
+//--------------------------------------------------------------
+void testApp::kill(){
+    // this is where the drone kills someone
 }
--- emptyExample/src/testApp.h
+++ emptyExample/src/testApp.h
@@ -4,24 +4,27 @@
   * HELLO VIEWER
   * HELLO READER
   *
-  * This performance is called Void Kill (). 
+  * This performance, installation, and softwarae is called Void Kill (). 
+  * It was created by Jamie Zigelbaum for the Spring Break Art Show in New York.
+  * http://jamiezigelbaum.com
+  *
   *
   * If you are here in person you will see me working at a desk with a computer, 
-  * keyboard, etc. writing some software in a gallery at the Spring Break Art Show in New York.
+  * keyboard, etc. writing some software in a gallery.
+  * 
   * Or maybe it's after the performance and you are just seeing the remains, reading a quiet screen.
   *
   * If you are reading this somewhere else, you are perhaps on github, looking at the code
-  * on your computer, or reading it some other way after the performance..
+  * on your computer, or reading it some other way after the performance.
   *
   * Github repo https://github.com/jamiezigelbaum/VoidKill
+  * 
+  * Everything here is speculation.
   *
   */
 
 
-
 #include "ofMain.h"
-
-
 
 
 class testApp : public ofBaseApp{
@@ -29,12 +32,24 @@ class testApp : public ofBaseApp{
   public:
     void setup();
     void update();
-		
+	
+	// flight operation and navigation stuff
     void preflightCheck();
     void takeoff();
     void exitAirspace();
     void setDestination();
+    void flyToDestination();
     
-
+    // target aquisition and analysis
+    void aquireTarget();
+    void identifyTarget();
+    void isTargetOnKillList();
+    void isTargetHostile();
+    void evaluateThreat();
+    
+    //
+    void kill();
+    
+    
     
 };
