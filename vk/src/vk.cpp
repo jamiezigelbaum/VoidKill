4c4cb469a7fa87f70d347400aaab5ac5fddef30d
Jamie Zigelbaum <jamie@zigelbaumcoelho.com>
2013-03-05 15:04:53 -0500

    renames the project

---
+++ vk/src/vk.cpp
@@ -0,0 +1,25 @@
+#include "vk.h"
+
+//--------------------------------------------------------------
+void vk::setup(){
+    // I assume in here I will get the drone ready to launch, weapons checks etc.
+
+}
+
+
+//--------------------------------------------------------------
+void vk::update(){
+
+}
+
+
+//--------------------------------------------------------------
+void vk::preflightCheck(){
+    // This seems a good thing, I'll make sure I won't explode when I take off
+}
+
+
+//--------------------------------------------------------------
+void vk::kill(){
+    // this is where the drone kills someone
+}
